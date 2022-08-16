b19bba438901f0540fef7e4b7718a781f13c734c
jehadmelad <jd.melad@gmail.com>
2022-08-16 16:21:54 +0200

    incclude dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3-alpine
+
+
+WORKDIR /app/
+
+COPY . .
+
+RUN pip3 install -r requirements.txt
+
+ENTRYPOINT [ "python3" ]
+
+CMD ["app.py" ]
+
--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from tracker import create_app
 
 
-
 app             =   create_app()
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5001, debug = True)
+    app.run('0.0.0.0', debug = True)
 
 
          
